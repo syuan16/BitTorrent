7c8c82a38b248f09f8f96e88fcd6344c38ea315e
syuan16 <wyuan29@wisc.edu>
2020-03-29 23:23:01 -0500

    Created tracker file

---
+++ tracker.go
@@ -0,0 +1,27 @@
+// the BitTorrent Tracker Protocol comes from https://wiki.theory.org/index.php/BitTorrent_Tracker_Protocol
+
+package BitTorrent
+
+import (
+	"net/url"
+	"strconv"
+)
+
+func (t *TorrentFile) buildTrackerURL(peerID [20]byte, port uint16) (string, error) {
+	base, err := url.Parse(t.Announce)
+	if err != nil {
+		return "", err
+	}
+
+	params := url.Values{
+		"info_hash":  []string{string(t.InfoHash[:])},
+		"peer_id":    []string{string(peerID[:])},
+		"port":       []string{strconv.Itoa(int(port))},
+		"uploaded":   []string{"0"},
+		"downloaded": []string{"0"},
+		"compact":    []string{"1"},
+		"left":       []string{strconv.Itoa(t.Length)},
+	}
+	base.RawQuery = params.Encode()
+	return base.String(), nil
+}
