6749ee88056eb1721c6bce6e9551ee03e6b9c19d
syuan16 <wyuan29@wisc.edu>
2020-03-29 22:39:49 -0500

    First Commit, Created torrent file parser

---
+++ TorrentFile.go
@@ -0,0 +1,85 @@
+package BitTorrent
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"github.com/jackpal/bencode-go"
+	"io"
+)
+
+type bencodeInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
+func (i *bencodeInfo) hash() ([20]byte, error) {
+	var buf bytes.Buffer
+	err := bencode.Marshal(&buf, &i)
+	if err != nil {
+		return [20]byte{}, err
+	}
+	h := sha1.Sum(buf.Bytes())
+	return h, nil
+}
+
+func (i *bencodeInfo) splitPieceHashes() ([][20]byte, error) {
+	hashLen := 20
+	buf := []byte(i.Pieces) // the byte array of the pieces of the info
+	if len(buf)%hashLen != 0 {
+		err := fmt.Errorf("Received malformed pieces of length %d", len(buf))
+		return nil, err
+	}
+	numHashes := len(buf) / hashLen
+	hashes := make([][20]byte, numHashes)
+
+	for i := 0; i < numHashes; i++ {
+		copy(hashes[i][:], buf[i*hashLen:(i+1)*hashLen])
+	}
+	return hashes, nil
+}
+
+func (bto *bencodeTorrent) toTorrentFile() (TorrentFile, error) {
+	infoHash, err := bto.Info.hash()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	pieceHashes, err := bto.Info.splitPieceHashes()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	t := TorrentFile{
+		Announce:    bto.Announce,
+		InfoHash:    infoHash,
+		PieceHashes: pieceHashes,
+		PieceLength: bto.Info.PieceLength,
+		Length:      bto.Info.Length,
+		Name:        bto.Info.Name,
+	}
+	return t, nil
+}
+
+func Open(r io.Reader) (*bencodeTorrent, error) {
+	bto := bencodeTorrent{}
+	err := bencode.Unmarshal(r, &bto)
+	if err != nil {
+		return nil, err
+	}
+	return &bto, nil
+}
