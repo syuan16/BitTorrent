05fa6b80efaad3f23a667c77d27ae8607484d438
syuan16 <wyuan29@wisc.edu>
2020-03-30 23:17:47 -0500

    Created peers.go file

---
+++ peers.go
@@ -0,0 +1,30 @@
+package BitTorrent
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+type Peer struct {
+	IP   net.IP
+	Port uint16
+}
+
+func Unmarshal(peersBin []byte) ([]Peer, error) {
+	const peerSize = 6 // 4 for IP, 2 for port
+	numPeers := len(peersBin) / peerSize
+	if len(peersBin)/peerSize != 0 {
+		err := fmt.Errorf("Received malformed peers")
+		return nil, err
+	}
+
+	peers := make([]Peer, numPeers)
+	for i := 0; i < numPeers; i++ {
+		offset := i * peerSize
+		peers[i].IP = net.IP(peersBin[offset : offset+4])
+		peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4 : offset+6])
+	}
+
+	return peers, nil
+}
