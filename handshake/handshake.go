e5e6a15ca8c3c309e71cca358d9989a0de248333
syuan16 <wyuan29@wisc.edu>
2020-04-10 17:46:31 -0500

    Created handshake.go file

---
+++ handshake/handshake.go
@@ -0,0 +1,67 @@
+package handshake
+
+import (
+	"fmt"
+	"io"
+)
+
+type Handshake struct {
+	Pstr     string
+	InfoHash [20]byte
+	PeerID   [20]byte
+}
+
+/*
+	this function serialize the handshake to a buffer
+*/
+func (h *Handshake) Serialize() []byte {
+	buf := make([]byte, len(h.Pstr)+49)
+	buf[0] = byte(len(h.Pstr))
+	curr := 1
+	curr += copy(buf[curr:], h.Pstr)
+	curr += copy(buf[curr:], make([]byte, 8))
+	curr += copy(buf[curr:], h.InfoHash[:])
+	curr += copy(buf[curr:], h.PeerID[:])
+	return buf
+}
+
+func Read(r io.Reader) (*Handshake, error) {
+	lengthBuf := make([]byte, 1)
+	var err error
+	_, err = io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+	pstrlen := int(lengthBuf[0])
+	if pstrlen == 0 {
+		err := fmt.Errorf("pstrlen cannot be 0")
+		return nil, err
+	}
+
+	handshakeBuf := make([]byte, 48+pstrlen)
+	_, err = io.ReadFull(r, handshakeBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	var infoHash, peerID [20]byte
+	copy(infoHash[:], handshakeBuf[pstrlen+8:pstrlen+8+20])
+	copy(peerID[:], handshakeBuf[pstrlen+28:])
+
+	h := Handshake{
+		Pstr:     string(handshakeBuf[0:pstrlen]),
+		InfoHash: infoHash,
+		PeerID:   peerID,
+	}
+
+	return &h, nil
+
+}
+
+func New(infoHash, peerID [20]byte) *Handshake {
+	return &Handshake{
+		Pstr:     "BitTorrent protocol",
+		InfoHash: infoHash,
+		PeerID:   peerID,
+	}
+}
