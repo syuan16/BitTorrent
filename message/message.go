b49880c25bf693dd38f68283777d564e4bc3693d
Stephen Yuan <wyuan29@wisc.edu>
2020-05-11 23:01:14 -0500

    Added functions for creating different messages

--- message/message.go
+++ message/message.go
@@ -20,7 +20,7 @@ const (
 )
 
 type Message struct {
-	ID messageID
+	ID      messageID
 	Payload []byte
 }
 
@@ -29,7 +29,7 @@ func (m *Message) Serialize() []byte {
 		return make([]byte, 4)
 	}
 	length := uint32(len(m.Payload) + 1) // +1 for id
-	buf := make([]byte, 4 + length)
+	buf := make([]byte, 4+length)
 	binary.BigEndian.PutUint32(buf[0:4], length)
 	buf[4] = byte(m.ID)
 	copy(buf[5:], m.Payload)
@@ -45,7 +45,6 @@ func Read(r io.Reader) (*Message, error) {
 
 	length := binary.BigEndian.Uint32(lengthBuf)
 
-
 	// keep-alive message
 	if length == 0 {
 		return nil, nil
@@ -53,7 +52,7 @@ func Read(r io.Reader) (*Message, error) {
 
 	messageBuf := make([]byte, length)
 	_, err = io.ReadFull(r, messageBuf)
-	if err := nil {
+	if err == nil {
 		return nil, err
 	}
 
@@ -63,9 +62,26 @@ func Read(r io.Reader) (*Message, error) {
 	}
 
 	return &m, nil
-
-
 }
 
+// create a REQUEST message
+func FormatRequest(index, begin, length int) *Message {
+	payload := make([]byte, 12)
+	binary.BigEndian.PutUint32(payload[0:4], uint32(index))
+	binary.BigEndian.PutUint32(payload[4:8], uint32(index))
+	binary.BigEndian.PutUint32(payload[8:12], uint32(index))
+	return &Message{
+		ID:      MsgRequest,
+		Payload: payload,
+	}
+}
 
-
+// creates a HAVE message
+func FormatHave(index int) *Message {
+	payload := make([]byte, 4)
+	binary.BigEndian.PutUint32(payload, uint32(index))
+	return &Message{
+		ID:      MsgHave,
+		Payload: payload,
+	}
+}
