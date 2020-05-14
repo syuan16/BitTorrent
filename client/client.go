8097107bf0c45d3a7b5c24cf14259103d37b82e5
Stephen Yuan <wyuan29@wisc.edu>
2020-05-14 15:19:59 -0500

    Added functions for creating new connection with peer

--- client/client.go
+++ client/client.go
@@ -1,5 +1,88 @@
 package client
 
-func New(peer peers.Peer) {
+import (
+	"BitTorrent/bitfield"
+	"BitTorrent/handshake"
+	"BitTorrent/message"
+	"BitTorrent/peers"
+	"bytes"
+	"fmt"
+	"net"
+	"time"
+)
 
+type Client struct {
+	Conn     net.Conn
+	Choked   bool
+	Bitfield bitfield.Bitfield
+	peer     peers.Peer
+	infoHash [20]byte
+	peerID   [20]byte
+}
+
+func completeHandshake(conn net.Conn, infoHash, peerID [20]byte) (*handshake.Handshake, error) {
+	conn.SetDeadline(time.Now().Add(3 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	req := handshake.New(infoHash, peerID)
+	_, err := conn.Write(req.Serialize())
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := handshake.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if !bytes.Equal(res.InfoHash[:], infoHash[:]) {
+		return nil, fmt.Errorf("expected infohash %x but got %x", res.InfoHash, infoHash)
+	}
+	return res, nil
+}
+
+func recvBitfield(conn net.Conn) (bitfield.Bitfield, error) {
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{}) // disable the deadline
+
+	msg, err := message.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if msg.ID != message.MsgBitfield {
+		err := fmt.Errorf("Expected bitfield but got ID %d", msg.ID)
+		return nil, err
+	}
+
+	return msg.Payload, nil
+}
+
+// New connects with a peer, completes a handshake and receives a handshake
+// returns an err if any of those fail
+func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
+	conn, err := net.DialTimeout("tcp", peer.String(), 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = completeHandshake(conn, infoHash, peerID)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	bf, err := recvBitfield(conn)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return &Client{
+		Conn:     conn,
+		Choked:   true,
+		Bitfield: bf,
+		peer:     peer,
+		infoHash: infoHash,
+		peerID:   peerID,
+	}, nil
 }
